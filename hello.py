fdab5a81416bd4ecd88eaeaa3804d81dec4692b0
没情绪 <3264330095@qq.com>
2025-12-24 17:43:11 +0800

    基础语法

---
+++ hello.py
@@ -0,0 +1,77 @@
+# print(True)
+# print(True+1)
+# print(False-1)
+# print(100)
+
+
+# 一个变量可以存储不同类型的数据
+# num=3.14
+# print(num)
+# num = num + 1
+# print(num)
+# num = "OK"
+# print(num)
+
+# a=10
+# b=20
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+# print(type("hello world"))
+# print(type(10))
+# print(type(True))
+# print(type(None))
+
+# print(isinstance(10,str))
+# print(isinstance(10,int))
+
+# s1="hello world"
+# s2='hello world'
+# s3="""
+# Hello
+# world
+# hello world
+# """
+# print(s1)
+# print(s2)
+# print(s3)
+
+# slogan="hello" "world"
+# print(slogan)
+# slogan="hello"+"world"
+# print(slogan)
+
+# s1="8"
+# s2="小时"
+# print(s1 + s2 + "学完python")
+
+# 格式化字符串
+# name="小王"
+# age=20
+# print("我的名字叫%s,今年%d岁"%(name,age))#占位符
+# print(f"我的名字叫{name},今年{age}岁")#f"...{变量名/表达式}...""
+# print("我的名字叫{0},今年{1}岁".format(name,age))#{0}表示第一个参数，{1}表示第二个参数
+
+
+#输入输出
+# name=input("请输入你的名字：")
+# age=input("请输入你的年龄：")
+# print("你的名字是："+name)
+# print("你的年龄是："+age)
+
+# 类
+# class Man:
+#     def __init__(self, name):
+#         self.name = name
+#         print("Initialized!")
+#     def hello(self):
+#         print("Hello " + self.name + "!")
+#     def goodbye(self):
+#         print("Good-bye " + self.name + "!")
+
+# m = Man("David")
+# m.hello()
+# m.goodbye()
+
