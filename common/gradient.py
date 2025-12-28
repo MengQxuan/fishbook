e022544c7bca38fb20f8ce604f8ad6076f50e1fd
没情绪 <3264330095@qq.com>
2025-12-28 19:21:33 +0800

    神经网络的梯度

--- common/gradient.py
+++ common/gradient.py
@@ -32,19 +32,27 @@ def numerical_gradient_2d(f, X):
 
 
 def numerical_gradient(f, x):
+    """
+    计算函数f在点x处的数值梯度
+    参数:
+        f: 待求梯度的函数，接受一个numpy数组作为输入，返回标量值
+        x: 输入点，numpy数组，可以是任意维度
+    返回值:
+        grad: 与输入x形状相同的numpy数组，包含函数f在点x处的梯度值
+    """
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
     
-    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite']) # 创建一个可以遍历多维数组的迭代器
     while not it.finished:
-        idx = it.multi_index
+        idx = it.multi_index # 获取当前元素的多维索引
         tmp_val = x[idx]
         x[idx] = float(tmp_val) + h
         fxh1 = f(x) # f(x+h)
         
         x[idx] = tmp_val - h 
         fxh2 = f(x) # f(x-h)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
+        grad[idx] = (fxh1 - fxh2) / (2*h) # 使用中心差分法
         
         x[idx] = tmp_val # 还原值
         it.iternext()   
